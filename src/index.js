36f48e7e760a9a668f9c39db12f3eb25b5f18dc0
liuhan <liuhann@gmail.com>
2020-07-24 15:17:39 +0800

    add restify db

--- src/index.js
+++ src/index.js
@@ -1,33 +1,35 @@
-const RestfyDB = require('./rest.js'),
-    packageInfo = require('../package.json'),
-    Producer = require('./producer'),
-    LowDB = require('./lowdb'),
-    Collection = require('./collection'),
-    DataBase = require('./database');
+const RestfyDB = require('./rest.js')
+const packageInfo = require('../package.json')
+const Producer = require('./producer')
+const LowDB = require('./lowdb')
+const Collection = require('./collection')
+const DBRestify = require('./rest')
+const DataBase = require('./database')
 
 module.exports = {
-    name: packageInfo.name,
-    version: packageInfo.version,
-    description: packageInfo.description,
-    Collection,
-    DataBase,
-    Producer,
-    // 模块初始化动作，对于核心模块可以进行koa相关插件注册
-    // 业务模块可以进行服务创建
-    async created(app) {
-        app.dataBaseProducer = new Producer();
+  name: packageInfo.name,
+  version: packageInfo.version,
+  description: packageInfo.description,
+  Collection,
+  DataBase,
+  Producer,
+  DBRestify,
+  // 模块初始化动作，对于核心模块可以进行koa相关插件注册
+  // 业务模块可以进行服务创建
+  async created (app) {
+    app.dataBaseProducer = new Producer()
 
-        // 默认设置Lowdb实现
-        app.dataBaseProducer.setDatabaseImpl(LowDB, app.lowdb)
-    },
+    // 默认设置Lowdb实现
+    app.dataBaseProducer.setDatabaseImpl(LowDB, app.lowdb)
+  },
 
-    // 模块路由注册，对外提供API可在此写api相关地址
-    async ready(app) {
-        await app.dataBaseProducer.ready(app);
+  // 模块路由注册，对外提供API可在此写api相关地址
+  async ready (app) {
+    await app.dataBaseProducer.ready(app)
 
-        new RestfyDB().initRoutes(app.router);
-    },
+    new RestfyDB().initRoutes(app.router)
+  },
 
-    // 启动收尾工作，可以在此执行建库、建索引等一些全局的具体业务操作
-    async bootComplete(app) { }
-};
+  // 启动收尾工作，可以在此执行建库、建索引等一些全局的具体业务操作
+  async bootComplete (app) { }
+}
--- src/rest.js
+++ src/rest.js
@@ -7,10 +7,31 @@ module.exports = class DBRestify {
     this.colName = colName
   }
 
-  async initRoute (router) {
-    const path = `${this.dbName}/${this.colName}`
+  async initRoute (app) {
+    const { router, logger } = app
+    // auto inject to app
+    if (!app.restdb) {
+      app.restdb = {}
+    }
+    if (app.restdb[this.dbName + '.' + this.colName]) {
+      console.warn(this.dbName + '.' + this.colName + ' already registered')
+      return
+    }
+    app.restdb[this.dbName + '.' + this.colName] = this
+    this.logger = logger
+    await this.initCollection(app)
+    const path = `restful/${this.dbName}/${this.colName}`
+    // 创建对象
+    router.post(`/${path}`, async (ctx, next) => {
+      const object = ctx.request.body
+      this.logger.debug('creating object [%o]', object)
+      object.creator = ctx.user ? ctx.user.id : ''
+      ctx.body = await this.create(ctx.request.body)
+      await next()
+    })
 
-    router.get(`${path}`, this.getCollection.bind(this), async (ctx, next) => {
+    // 列表查询
+    router.get(`/${path}`, async (ctx, next) => {
       const skip = parseInt(ctx.request.query.skip) || 0
       const limit = parseInt(ctx.request.query.limit) || 20
       const sort = ctx.request.query.sort
@@ -23,102 +44,125 @@ module.exports = class DBRestify {
       delete query.order
       delete query.projection
 
-      ctx.body = await this.list(query, {
+      const options = {
         skip,
-        limit,
-        sort: {
+        limit
+      }
+      if (sort) {
+        options.sort = {
           [sort]: parseInt(order)
-        },
-        projection
-      })
+        }
+      }
+      if (projection) {
+        const projects = projection.split(',')
+        options.projection = {}
+        for (const p of projects) {
+          options.projection[p] = 1
+        }
+      }
+      this.logger.debug('query %s  query=%o options=%o', path, query, options)
+
+      ctx.body = await this.list(query, options)
       await next()
     })
 
-    router.get(`${path}/:id`, async (ctx, next) => {
-      const { id } = ctx.request.params
+    // 按id获取
+    router.get(`/${path}/:id`, async (ctx, next) => {
+      const { id } = ctx.params
+
+      this.logger.debug('findOne id=%s', id)
       ctx.body = await this.getDocument(id)
       await next()
     })
 
-    router.post(`${path}/id`, async (ctx, next) => {
-      const { ids } = ctx.query.body
-    })
-    router.post(`${path}/distinct/:field`, this.distinct.bind(this))
-    router.get(`${path}/regex/:prop/:value`, this.regex.bind(this))
-    const middleware = filter || (async (ctx, next) => {
+    // 多个查找
+    router.post(`/${path}/id`, async (ctx, next) => {
+      const { ids } = ctx.request.body
+
+      ctx.body = await this.getMultipleDocuments(ids)
       await next()
     })
-    router.post(`${path}`, middleware, this.create.bind(this))
-    router.patch(`${path}/:id`, middleware, this.patch.bind(this))
-    router.delete(`${path}/:id`, middleware, this.delete.bind(this))
-    debug('rest service booted ' + path)
+
+    // 字段投影
+    router.get(`/${path}/distinct/:field`, async (ctx, next) => {
+      const { field } = ctx.params
+
+      ctx.body = await this.distinct(field, ctx.request.query)
+
+      await next()
+    })
+
+    // 复杂搜索
+    router.post(`/${path}/search`, async (ctx, next) => {
+      const skip = parseInt(ctx.request.query.skip) || 0
+      const limit = parseInt(ctx.request.query.limit) || 20
+      const sort = ctx.request.query.sort
+      const order = ctx.request.query.order
+      const projection = ctx.request.query.projection
+      const query = Object.assign({}, ctx.request.body)
+
+      const options = {
+        skip,
+        limit
+      }
+      if (sort) {
+        options.sort = {
+          [sort]: parseInt(order)
+        }
+      }
+      if (projection) {
+        const projects = projection.split(',')
+        options.projection = {}
+        for (const p of projects) {
+          options.projection[p] = 1
+        }
+      }
+      this.logger.debug('query %s  query=%o options=%o', path, query, options)
+
+      ctx.body = await this.list(query, options)
+      await next()
+    })
+
+    // 更新字段
+    router.patch(`/${path}/:id`, async (ctx, next) => {
+      const { id } = ctx.params
+      ctx.body = await this.patch(id, ctx.request.body)
+
+      await next()
+    })
+
+    // 单个删除
+    router.delete(`/${path}/:id`, async (ctx, next) => {
+      const { id } = ctx.params
+      ctx.body = await this.delete(id)
+
+      await next()
+    })
+
+    // 批量删除
+    router.delete(`/${path}`, async (ctx, next) => {
+      const deleteList = ctx.request.body
+      ctx.body = await this.delete(deleteList)
+      await next()
+    })
+
+    app.info(`${this.dbName}/${this.colName} restful enabled`)
+
+    return `${this.dbName}/${this.colName}`
   }
 
-  async getCollection (ctx, next) {
+  async initCollection (app) {
     if (!this.coll) {
-      const db = await ctx.getDb(this.dbName)
+      const db = await app.getDb(this.dbName)
       this.coll = await db.getCollection(this.colName)
     }
-    await next()
   }
 
   setAdmin (admin) {
     this.admin = admin
   }
 
-  async ensureIndex (indexKey, {
-    overwriteOnDuplicated
-  }) {
-    const db = await this.getDb()
-    await db.collection(this.coll).createIndex({
-      [indexKey]: 1
-    }, {
-      unique: true
-    })
-    this.indexKey = indexKey
-    debug(`ensure index ${this.coll}: ${indexKey}`)
-    this.overwriteOnDuplicated = overwriteOnDuplicated
-  }
-
-  /**
-       * 设置自动转换为ObjectId类型的字段
-       * @param keys
-       */
-  async setObjectIdField (keys) {
-    this.objectIdFields = keys
-  }
-
-  /**
-       * 设置子集和对应的外键， 主要用于目录、文件这类包含子项应用场景
-       * @param subColl 子集名称
-       * @param subCollForeignKey  子集文档外键 指向当前集合的id
-       * @param collKey 父文档标记字段，默认为_id
-       * @returns {Promise<void>}
-       */
-  setSubCollection (subColl, subCollForeignKey, collKey) {
-    this.subColl = subColl
-    this.subCollForeignKey = subCollForeignKey
-    this.parentCollKey = collKey
-  }
-
-  /**
-       * 正则查找
-       */
-  async regex (ctx, next) {
-    const db = await this.getDb()
-    const coll = db.collection(this.coll)
-    const count = ctx.request.query.count || 1000
-    const query = {}
-    query[ctx.params.prop] = {
-      $regex: ctx.params.value
-    }
-    const cursor = await coll.find(query).limit(count)
-    const result = await cursor.toArray()
-    ctx.body = {
-      result
-    }
-    await next()
-  }
+  async ensureIndex (indexKey) {}
 
   async list (query, options) {
     // consider value convert to true or false
@@ -129,8 +173,6 @@ module.exports = class DBRestify {
     }
     const list = await this.coll.find(query, options)
     const total = await this.coll.count(query)
-    const cursor = coll.find(query)
-
     return {
       query,
       options,
@@ -140,70 +182,24 @@ module.exports = class DBRestify {
   }
 
   /**
-       * 创建一条文档，如何发生主键重复时， overwriteOnDuplicated 可配置是否可替换
-       * @param ctx
-       * @param next
-       * @returns {Promise<void>}
-       */
-  async create (ctx, next) {
-    const object = ctx.request.body
-    object.creator = ctx.user.id
-    object.created = new Date().getTime()
-    object.updated = object.created
-    object.token = ctx.token
-    const db = await this.getDb()
-    const coll = db.collection(this.coll)
-    let result = null
-    this.convertForeignFieldValueToObjectId(ctx, object)
-    for (const key in object) {
-      if (key.match(/^_[a-z]+_id$/)) {
-        object[key] = new ObjectID(object[key])
+   * 创建一条文档，如何发生主键重复时， overwriteOnDuplicated 可配置是否可替换
+   * @param ctx
+   * @param next
+   * @returns {Promise<void>}
+   */
+  async create (object) {
+    if (object._id) {
+      const existed = await this.coll.exist(object._id)
+      if (existed) {
+        return await this.coll.update(object._id, object)
       }
     }
-    try {
-      result = await coll.insertOne(object, {
-        bypassDocumentValidation: true
-      })
-    } catch (e) {
-      if (e instanceof MongoError) {
-        if (this.overwriteOnDuplicated) {
-          debug(`overwrite duplicated  ${this.coll} ${this.indexKey}=${object[this.indexKey]}`)
-          await coll.deleteOne({
-            [this.indexKey]: object[this.indexKey]
-          })
-          result = await coll.insertOne(object)
-          debug('removed and inserted new')
-        }
-      }
-    }
-    ctx.body = {
-      result,
-      object
-    }
-    await next()
-  }
-
-  convertForeignFieldValueToObjectId (ctx, object) {
-    // 处理foreignkey 创建转换
-    try {
-      if (this.objectIdFields) {
-        for (const foreignKey of this.objectIdFields) {
-          if (object[foreignKey]) {
-            object[foreignKey] = new ObjectID(object[foreignKey])
-          }
-          if (foreignKey.match(/^_[a-z]+_id$/)) {
-            object[foreignKey] = new ObjectID(object[foreignKey])
-          }
-        }
-      }
-    } catch (e) {
-      ctx.throw(400, this.objectIdFields + ' must be ObjectId')
-    }
+    return await this.coll.insert(object)
   }
 
   async getMultipleDocuments (ids) {
     return await this.coll.find({
-      id: {
+      _id: {
         $in: ids
       }
     })
@@ -214,91 +210,26 @@ module.exports = class DBRestify {
   }
 
   /**
-       * 更新文档
-       * @param ctx
-       * @param next
-       * @returns {Promise<void>}
-       */
-  async patch (ctx, next) {
-    const body = ctx.request.body
-    const db = await this.getDb()
-    const coll = db.collection(this.coll)
-    const setProperties = Object.assign({}, body)
-
-    // only admin can modify prop.system
-    if (setProperties.system && ctx.user.id !== this.admin) {
-      debug(`Patch prop.system ${ctx.user.id} !== ${this.admin}`)
-      ctx.throw(403, 'Patch on prop.system not allowed')
-      return
-    }
-    setProperties.updated = new Date().getTime()
-    if (setProperties._id) {
-      delete setProperties._id
-    }
-    const objectId = ctx.params.id
-
-    setProperties.packKey = new ObjectID(objectId)
-
-    await coll.findOneAndUpdate({
-      _id: new ObjectID(objectId)
-    }, {
-      $set: setProperties
-    })
-    ctx.body = {
-      code: 204
-    }
-    await next()
+         * 更新文档
+         * @param ctx
+         * @param next
+         * @returns {Promise<void>}
+         */
+  async patch (id, patch) {
+    return await this.coll.patch(id, patch)
   }
 
-  async delete (ctx, next) {
-    const objectId = ctx.params.id
-    const db = await this.getDb()
-    const coll = db.collection(this.coll)
-    const found = await coll.findOne({
-      _id: new ObjectID(objectId)
-    })
-    if (found) {
-      // 匿名删除
-      if ((found.creator == null && ctx.token === found.token) || found.token == null) {
-        const deleted = await coll.deleteOne({
-          _id: new ObjectID(objectId)
-        })
-        ctx.body = {
-          deleted
-        }
-        await next()
-        return
-      }
-
-      if (!ctx.user || !ctx.user.id) { // only the creator and admin can perform deleting
-        ctx.throw(403)
-      }
-      if (found.creator === ctx.user.id || ctx.user.id === this.admin) {
-        const deleted = await coll.deleteOne({
-          _id: new ObjectID(objectId)
-        })
-        ctx.body = {
-          deleted
-        }
-      } else {
-        ctx.throw(403)
-      }
-    }
-    await next()
+  async delete (id) {
+    return await this.coll.remove(id)
   }
 
   /**
-       * 获取coll的distinct列值
-       * @param ctx
-       * @param next
-       * @returns {Promise<void>}
-       */
-  async distinct (ctx, next) {
-    const { field } = ctx.params
-    const db = await this.getDb()
-    const coll = db.collection(this.coll)
-    const distinctValue = await coll.distinct(field, ctx.request.body)
-    ctx.body = distinctValue
-    await next()
+         * 获取coll的distinct列值
+         * @param ctx
+         * @param next
+         * @returns {Promise<void>}
+         */
+  async distinct (field) {
+    return await this.coll.distinct(field)
   }
 }
